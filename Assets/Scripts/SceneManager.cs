181819fb4d3a0cde0dd918910d5371b367f14c1a
Pixels <pixels671@protonmail.com>
2025-02-02 18:17:26 +0300

    Add basic title screen

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public static SceneManager I { get; private set; }
+    
+    private void Awake() {
+        if (I == null) {
+            I = this;
+            DontDestroyOnLoad(this);
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadNextScene() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(
+            UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1
+        );
+    }
+}
