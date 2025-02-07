d5c80950d95f7d4e976eed52cc02db34a81cd44e
rassoc98 <samcossar@outlook.com>
2025-02-07 10:38:36 +0000

    Add Level Select functionality to scene manager

--- Assets/Scripts/Managers/SceneManager.cs
+++ Assets/Scripts/Managers/SceneManager.cs
@@ -1,25 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class SceneManager : MonoBehaviour {
+public class SceneManager : MonoBehaviour
+{
     public static SceneManager I { get; private set; }
 
-    private void Awake() {
+
+    [Header("Level Selection")]
+    public LevelSelectButton[] levels;
+    public List<LevelSelectButton> unlockedLevels;
+
+    private void Awake()
+    {
         if (I == null)
             I = this;
         else
             Destroy(gameObject);
     }
 
-    public void LoadNextScene() {
+    private void Start()
+    {
+        //StartLevelSelect();
+    }
+
+    public void LoadNextScene()
+    {
         UnityEngine.SceneManagement.SceneManager.LoadScene(
             UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1
         );
     }
 
-    public void QuitGame() {
+    public void QuitGame()
+    {
         Application.Quit();
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
-        #endif
+#endif
     }
+
+    #region Level Selection
+    public void StartLevelSelect()
+    {
+        LoadPlayerPrefs();
+    }
+    public void OnLevelComplete(Scene scene) // call this with context of the current scene
+    {
+        List<string> list = new();
+        foreach (LevelSelectButton s in levels) list.Add(s.SceneID);
+
+        if (list.IndexOf(scene.name) == list.Count - 1)
+            return; //maybe could add some "you completed the game" scene
+        else if (list.IndexOf(scene.name) == -1)
+        {
+            Debug.LogError("next level not found, check SceneManager inspector for errors");
+            return;
+        }
+
+        var newLevel = levels[list.IndexOf(scene.name) + 1];
+        unlockedLevels.Add(newLevel);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(nameof(newLevel)); //  currently takes you to the new level, could also take you back to main menu
+        SavePlayerPrefs();
+    }
+    public void LockButton(LevelSelectButton subject)
+    {
+        subject.button.GetComponent<Button>().enabled = false;
+        subject.button.GetComponent<Image>().enabled = false;
+    }
+    public void UnlockButton(LevelSelectButton subject)
+    {
+        subject.button.GetComponent<Button>().enabled = true;
+        subject.button.GetComponent<Image>().enabled = true;
+    }
+    public void OnLevelSelected(Scene scene)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(nameof(scene));
+    }
+    private void SavePlayerPrefs()
+    {
+        PlayerPrefs.SetInt("unlockCount", unlockedLevels.Count);
+    }
+    private void LoadPlayerPrefs()
+    {
+        foreach (LevelSelectButton lsb in levels)
+        {
+            LockButton(lsb);
+        }
+
+        if (!PlayerPrefs.HasKey("unlockCount")) PlayerPrefs.SetInt("unlockCount", 1);
+
+        unlockedLevels = new List<LevelSelectButton>();
+        for (int i = 0; i < PlayerPrefs.GetInt("unlockCount"); i++)
+        {
+            unlockedLevels.Add(levels[i]);
+        }
+        foreach (LevelSelectButton lsb in unlockedLevels)
+        {
+            UnlockButton(lsb);
+        }
+    }
+    #endregion
+}
+
+[System.Serializable]
+public class LevelSelectButton
+{
+    public string SceneID;
+    public GameObject button;
 }
