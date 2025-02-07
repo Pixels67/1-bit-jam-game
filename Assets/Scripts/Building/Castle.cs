3a05e246ec85e1de84bf65ce937dae1f8a436097
Pixels <pixels671@protonmail.com>
2025-02-07 21:11:43 +0300

    Add first 4 levels

--- Assets/Scripts/Building/Castle.cs
+++ Assets/Scripts/Building/Castle.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
 
 public class Castle : MonoBehaviour, IDamageable {
+    public int CurrentHealth { get; private set; }
+    
     public uint snow;
     public uint maxHealth;
-    
+
+    [SerializeField] private float snowGainRate;
     [SerializeField] private AudioClip hitSFX;
+    
+    private float m_snowTimer;
 
     private void Awake() {
         CurrentHealth = (int)maxHealth;
     }
 
-    public int CurrentHealth { get; private set; }
+    private void Update() {
+        m_snowTimer += Time.deltaTime;
+        if (m_snowTimer >= 1f / snowGainRate) {
+            snow += 1;
+            m_snowTimer = 0f;
+        }
+    }
 
     public void TakeDamage(uint damage) {
         CurrentHealth -= (int)damage;
